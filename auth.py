22a67542a07fa606bd8f6463d5e8a686f35636d9
firestso2 <firestso2@gmail.com>
2026-04-22 18:01:28 +0300

    Delete auth.py

--- auth.py
+++
@@ -1,7 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class AuthStates(StatesGroup):
-    waiting_phone    = State()
-    waiting_code     = State()
-    waiting_password = State()
-    authenticated    = State()
