de61befe2ff026a274d0408fb3e1851424743196
firestso2 <firestso2@gmail.com>
2026-04-24 09:28:57 +0300

    auth.py

--- states/auth.py
+++ states/auth.py
@@ -1,6 +1,8 @@
 from aiogram.fsm.state import State, StatesGroup
 
+
 class AuthStates(StatesGroup):
+    choosing_lang    = State()
     waiting_phone    = State()
     waiting_code     = State()
     waiting_password = State()
